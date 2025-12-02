679eda7771636eea463821869522e0f326d663d3
AbdulSamad94 <abdulsamadwork109@gmail.com>
2025-12-03 00:47:35 +0500

    env name changed to DOCUSAURUS_API_URL

--- src/components/Chatbot/index.tsx
+++ src/components/Chatbot/index.tsx
@@ -48,7 +48,7 @@ export default function Chatbot() {
     setIsLoading(true)
 
     try {
-      const response = await fetch(`${process.env.NEXT_API_URL}/api/chat`, {
+      const response = await fetch(`${process.env.DOCUSAURUS_API_URL}/api/chat`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
